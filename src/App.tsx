445ab03966a0e1d62c436fb8b84f628ad9562376
oenus777 <takatunejp@gmail.com>
2022-12-28 02:27:54 +0000

    [add] Footerコンポーネントにfooterを閉じ込める

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import * as React from 'react'
 
 import AppBar from './AppBar'
+import Footer from './Footer'
 
 // 1. import `ChakraProvider` component
 import { ChakraProvider, Box, Heading, Container } from '@chakra-ui/react'
@@ -16,9 +17,7 @@ export default function App() {
           <Heading>TODO管理</Heading>
           作成したTODO一覧が表示されます
         </Box>
-        <Box bg='#d3d3d3' textAlign='center' p='6' color='white'>
-          ©︎素振React
-        </Box>
+        <Footer />
       </Container>
     </ChakraProvider>
   )
---
+++ src/Footer.tsx
@@ -0,0 +1,11 @@
+import * as React from 'react'
+
+import { Box } from '@chakra-ui/react'
+
+export default function Footer() {
+    return (
+        <Box bg='#d3d3d3' textAlign='center' p='6' color='white'>
+          ©︎素振React
+        </Box>
+    )
+}
