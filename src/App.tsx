e35ab5e4f4c075402202dc847ecde9a4e6e3b134
oenus777 <takatunejp@gmail.com>
2022-12-28 02:12:10 +0000

    [update] Chakra UIを用いて簡単に修正してみる

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,24 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import * as React from 'react'
 
-function App() {
+// 1. import `ChakraProvider` component
+import { ChakraProvider, Box, Heading, Container } from '@chakra-ui/react'
+
+export default function App() {
+  // 2. Wrap ChakraProvider at the root of your app
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <ChakraProvider>
+      <Container maxW='100%' maxH='100%' p='0'>
+        <Box bg='#008080' w='100%' p={4} color='white'>
+          素振React
+        </Box>
+        <Box m='4'>
+          <Heading>TODO管理</Heading>
+          作成したTODO一覧が表示されます
+        </Box>
+        <Box bg='#d3d3d3' textAlign='center' p='6' color='white'>
+          ©︎素振React
+        </Box>
+      </Container>
+    </ChakraProvider>
+  )
 }
-
-export default App;
